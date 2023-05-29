de57e172a073a5f42b8665b4df12c96e205b5912
niyaazparker <niyaaz.parker@media24.com>
2023-05-29 11:13:11 +0200

    updated styling, adding expanding and collpasing to cards, updated follow,block button logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
   const [users, setUsers] = useState([]);
@@ -36,9 +38,13 @@ const App = () => {
   };
 
   const handleFollowUser = (userId) => {
-    if (!followedUsers.includes(userId)) {
+    if (followedUsers.includes(userId)) {
+      setFollowedUsers((prevFollowedUsers) => prevFollowedUsers.filter((id) => id !== userId));
+    } else {
       setFollowedUsers((prevFollowedUsers) => [...prevFollowedUsers, userId]);
     }
+    
+    setBlockedUsers((prevBlockedUsers) => prevBlockedUsers.filter((id) => id !== userId));
   };
 
   const handleBlockUser = (userId) => {
@@ -73,48 +79,58 @@ const App = () => {
           onChange={handleSearch}
         />
       </div>
-      <ul className="list-group mt-2">
+        <div className='row mt-4'>
         {currentUsers.map((user) => (
-          <li className="list-group-item" key={user.user_id}>
-          <div className="d-flex align-items-center">
-            <img src={user.profile_image} alt={user.display_name} className="mr-3" />
-            <span>{user.display_name}</span>
-            <div>
-            {!isUserFollowed(user.user_id) && (
-                <button className={`btn ${isUserFollowed(user.user_id) ? 'btn-success' : 'btn-primary'} ml-auto`}
-                onClick={() => handleFollowUser(user.user_id)}>
-                  Follow
-                </button>
-              )}
-              {!isUserBlocked(user.user_id) && (
-                <button className="btn btn-danger ml-2" onClick={() => handleBlockUser(user.user_id)}>
-                  Block
-                </button>
-              )}
-              {isUserBlocked(user.user_id) && (
-                <button className="btn btn-secondary ml-2" disabled>
-                  Blocked
-                </button>
-              )}
+          <div className='col-lg-4 col-md-6 col-sm-12 col-12 mb-4'>
+          <div className="card p-4" key={user.user_id}>
+            <div className="image d-flex flex-column justify-content-center align-items-center">
+              <img src={user.profile_image} alt={user.display_name} height="100" width="100" />
             </div>
+            <span className='name mt-3 text-center'>{user.display_name}</span>
+            <div className='gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center'>
+                <button
+                  className={`btn ${isUserFollowed(user.user_id) ? 'btn-success' : 'btn-primary'} ml-auto`}
+                  onClick={() => handleFollowUser(user.user_id)}
+                >
+                  {isUserFollowed(user.user_id) ? 'Unfollow' : 'Follow'}
+                </button>
+                {!isUserBlocked(user.user_id) && (
+                  <button className="btn btn-danger ml-2" onClick={() => handleBlockUser(user.user_id)}>
+                    Block
+                  </button>
+                )}
+                {isUserBlocked(user.user_id) && (
+                  <button className="btn btn-secondary ml-2">
+                    Blocked
+                  </button>
+                )}
+              </div>
+            
             <button
-              className="btn btn-link ml-auto"
+              className="btn btn-link ml-auto pt-3 pb-0"
               onClick={() => handleExpandCollapse(user.user_id)}
               disabled={isUserBlocked(user.user_id)}
             >
-              {expandedUser === user.user_id ? 'Collapse' : 'Expand'}
+                {expandedUser === user.user_id ? (
+                  <FontAwesomeIcon icon={faChevronUp} />
+                ) : (
+                  <FontAwesomeIcon icon={faChevronDown} />
+                )}
             </button>
-          </div>
-          {expandedUser === user.user_id && (
-            <div className={`mt-2 ${expandedUser === user.user_id ? 'expand-content' : 'collapse-content'}`}>
-              <p>Reputation: {user.reputation}</p>
-              <p>Location: {user.location}</p>
-              <p>Website: {user.website_url}</p>
+
+            {expandedUser === user.user_id && (
+            <div className={`mt-2 border-top pt-3 mt-3 ${expandedUser === user.user_id ? 'expand-content ' : 'collapse-content'}`}>
+              <p className='mb-1'>Reputation: {user.reputation}</p>
+              <p className='mb-1'>Location: {user.location}</p>
+              <p className='mb-0'>Website: {user.website_url}</p>
             </div>
           )}
-        </li>
+          </div>
+          
+        </div>
         ))}
-      </ul>
+      </div>
+     
       <nav className="mt-4">
         <ul className="pagination justify-content-center">
           {Array(Math.ceil(filteredUsers.length / usersPerPage))
